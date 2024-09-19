a8f94fd754792ba36a0f5aa7ff3097a0d7a534bf
Toby Ceballos <96705059+TobyCeballos@users.noreply.github.com>
2024-09-19 14:38:36 -0300

    Update index.tsx

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -27,27 +27,15 @@ const Banner = () => {
             </h3>
 
             <div className="relative text-white focus-within:text-white flex flex-row-reverse input-shadow rounded-full pt-5 lg:pt-0">
-              <input
-                type="text"
-                name="q"
-                className="py-6 lg:py-8 text-lg w-full text-black opacity-75 rounded-full pl-8 focus:outline-none focus:text-black bg-gray-700 placeholder-gray-400"
-                placeholder="search courses..."
-                autoComplete="off"
-              />
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 pt-5 lg:pt-0">
-                <button
-                  type="submit"
-                  className="p-3 lg:p-5 focus:outline-none focus:shadow-outline bg-teal-500 hover:bg-teal-400 duration-150 ease-in-out rounded-full"
-                >
-                  <Image
-                    src={"/assets/banner/search.svg"}
-                    alt="inputicon"
-                    width={30}
-                    height={30}
-                  />
-                </button>
-              </div>
-            </div>
+  <div className="absolute inset-y-0 right-0 flex items-center pr-2 pt-5 lg:pt-0">
+    <button
+      type="button"
+      className="p-3 lg:p-5 focus:outline-none focus:shadow-outline bg-teal-500 hover:bg-teal-400 duration-150 ease-in-out rounded-full"
+    >
+      Contactarse
+    </button>
+  </div>
+</div>
           </div>
 
           <div className="col-span-6 flex justify-center">
